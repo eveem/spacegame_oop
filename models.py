d82a9e4d1f94a794c4dadff8d97f2e81a4ac9548
eveem <monpriya.tammavong@gmail.com>
2016-12-02 14:50:18 +0700

    Create class Model

--- models.py
+++ models.py
@@ -1,14 +1,18 @@
 import arcade.key
 
-class Ship:
-	DIR_HORIZONTAL = 0
-	DIR_VERTICAL = 1
-
-	def __init__(self, world, x, y):
+class Model:
+	def __init__(self, world, x, y, angle):
 		self.world = world
 		self.x = x
 		self.y = y
 		self.angle = 0
+
+class Ship(Model):
+	DIR_HORIZONTAL = 0
+	DIR_VERTICAL = 1
+
+	def __init__(self, world, x, y):
+		super().__init__(world, x, y, 0)
 		self.direction = Ship.DIR_VERTICAL
 
 	def switch_direction(self):
@@ -35,6 +39,7 @@ class World:
 		self.height = height
 
 		self.ship = Ship(self, 100, 100)
+		self.gold = Gold(self, 400, 400)
 
 	def animate(self, delta):
 		self.ship.animate(delta)
@@ -42,3 +47,7 @@ class World:
 	def on_key_press(self, key, key_modiriers):
 		if key == arcade.key.SPACE:
 			self.ship.switch_direction()
+
+class Gold(Model):
+	def __init__(self, world, x, y):
+		super().__init__(world, x, y, 0)
