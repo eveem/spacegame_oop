f82117048995e44a9b8b07fae897f9eab970e5c3
eveem <monpriya.tammavong@gmail.com>
2016-12-02 13:33:44 +0700

    Draw ship complete

--- space.py
+++ space.py
@@ -4,16 +4,17 @@ SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
  
 class SpaceGameWindow(arcade.Window):
-    def __init__(self, width, height):
-        super().__init__(width, height)
+	def __init__(self, width, height):
+		super().__init__(width, height)
+		arcade.set_background_color(arcade.color.BLACK)
+		
+		self.ship = arcade.Sprite('images/ship.png')
+		self.ship.set_position(100, 100)
  
-        arcade.set_background_color(arcade.color.BLACK)
- 
- 
-    def on_draw(self):
-        arcade.start_render()
- 
+	def on_draw(self):
+		arcade.start_render()
+		self.ship.draw()
  
 if __name__ == '__main__':
-    window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
-    arcade.run()
+	window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+	arcade.run()
